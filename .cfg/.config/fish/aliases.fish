d7af7aaf331a9692795853f15e1793874f71c251
yourlocalautist <thenamessummer@proton.me>
2024-10-12 14:46:30 +0100

    silly commit

---
+++ .cfg/.config/fish/aliases.fish
@@ -0,0 +1,14 @@
+
+
+
+
+
+# starship modules
+alias toggle-user='starship toggle username'
+alias toggle-host='starship toggle hostname'
+alias toggle-dir='starship toggle directory'
+alias toggle-git='starship toggle git_branch'
+alias toggle-duration='starship toggle cmd_duration'
+
+# silly :3
+alias nekofetch="fastfetch --logo-height 6 -l ~/.config/fastfetch/nyarch.png"
